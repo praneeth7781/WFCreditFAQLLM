32db90d8dff9ae00ddc1fee734816bde2d79f316
Hamsika <hamsika857@gmail.com>
2025-01-23 00:27:00 +0530

    added UI Files

---
+++ float_frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Chatbot from './components/chatbot';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Chatbot/>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
